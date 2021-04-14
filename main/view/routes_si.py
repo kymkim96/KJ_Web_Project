cd625b886e6e137b8a43d2f28fcc1f1f4639bf5d
unknown <kymkim96@naver.com>
2021-04-14 09:59:29 +0900

    .

--- main/view/routes_si.py
+++ main/view/routes_si.py
@@ -168,7 +168,7 @@ def delivery():
         'nation': None
     }, classification_list=enumerate([
         ('컨테이너', 'IMP_CONTAINER_FLAG_19'),
-        ('운송조건', 'IMP_COUNTRY_OF_ORIGIN_16'),
+        ('운송조건', 'DEL_DELIVERY_TERM_CODE_20'),
         ('운송장소', 'DEL_PLACE_OF_DELIVERY_20'),
         ('국경에서의 운송 수단', 'IMP_INLAND_TRANSPORT_MODE_25'),
         ('국내 교통 수단', 'IMP_TRANSPORT_MODE_AT_BODR_26')
